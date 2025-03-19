feeb92c52d2543613fcc6e0c1c560ae519bc7c98
greetingsForAlek <alexpwoods@outlook.com>
2025-03-19 20:39:42 +0000

    finishing touches

--- src/main.js
+++ src/main.js
@@ -29,10 +29,6 @@ pointLight.position.set(5,5,5);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
-
 const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
@@ -45,10 +41,49 @@ function addStar() {
   star.position.set(x, y, z);
   scene.add(star);
 }
+const moonTexture = new THREE.TextureLoader().load('/PAPERPLANET.jpg');
+const normalTexture = new THREE.TextureLoader().load('/normal.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
+);
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+const mrpaperTexture = new THREE.TextureLoader().load('/Paper.jpg');
+
+const mrpaper = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({ map: mrpaperTexture })
+);
+
+scene.add(mrpaper);
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  mrpaper.rotation.x += 0.05;
+  mrpaper.rotation.y += 0.075;
+  mrpaper.rotation.z += 0.05;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+}
+document.body.onscroll = moveCamera;
 
 Array(200).fill().forEach(addStar);
 
-const spaceTexture = new THREE.TextureLoader().load('/Sky.jpg');
+const spaceTexture = new THREE.TextureLoader().load('/space.jpg');
 scene.background = spaceTexture;
 
 function animate() {
