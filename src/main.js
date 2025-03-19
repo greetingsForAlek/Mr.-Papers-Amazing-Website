a503a80743edfefe35b804985436170286847997
greetingsForAlek <alexpwoods@outlook.com>
2025-03-19 19:21:38 +0000

    background

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './style.css';
 import * as THREE from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { array } from 'three/tsl';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -34,6 +35,21 @@ scene.add(lightHelper, gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load('/Sky.jpg');
+scene.background = spaceTexture;
 
 function animate() {
   requestAnimationFrame(animate);
