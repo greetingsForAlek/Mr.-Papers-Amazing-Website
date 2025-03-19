0d9c746ec8b8a1a6087ab3675455c9b05bcadf8c
greetingsForAlek <alexpwoods@outlook.com>
2025-03-19 19:05:24 +0000

    orbit controls

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './style.css';
 
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
@@ -30,6 +32,8 @@ const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(lightHelper, gridHelper);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+
 
 function animate() {
   requestAnimationFrame(animate);
@@ -37,6 +41,8 @@ function animate() {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  controls.update();
 
   renderer.render(scene, camera);
 };
